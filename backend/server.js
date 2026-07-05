80696c87f90b2ca76a4bf09e3272954fc3858b96
a20n21 <andersonmatias188@outlook.com>
2026-07-05 16:16:48 -0300

    Argo + Observabilidade + Istio e deployment

--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,28 @@ import authRoutes from './src/routes/auth.routes.js';
 
 const app = express();
 
-// Middlewares
 app.use(cors());
 app.use(express.json());
 
-// Rotas
+
+app.get('/health', (req, res) => {
+    res.status(200).send('OK');
+});
+
+
+app.get('/ready', async (req, res) => {
+    try {
+        await pool.query('SELECT 1'); 
+        res.status(200).send('Ready');
+    } catch (err) {
+        res.status(503).send('Database not reachable');
+    }
+});
+
+
+
 app.use('/api/auth', authRoutes);
 
-// Rota de teste do banco (opcional, mas bom para debug)
 app.get('/test-db', async (req, res) => {
     try {
         const result = await pool.query('SELECT NOW()');
@@ -22,7 +36,6 @@ app.get('/test-db', async (req, res) => {
     }
 });
 
-// Inicialização
 app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000');
 });
