159658af5165e977c8ca89d040993fe406fb3fd5
a20n21 <andersonmatias188@outlook.com>
2026-07-06 23:22:10 -0300

    fix: reestrutura istio e sincroniza com main

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -6,7 +6,7 @@ export const login = async (req, res) => {
     const { email, password } = req.body;
 
     if (!email || !password) {
-        return res.status(400).json({ error: "Email e senha são obrigatórios" });
+        return res.status(400).json({ error: "Email e senha serão obrigatórios" });
     }
 
     try {
