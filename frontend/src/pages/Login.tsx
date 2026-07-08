91b049bbbb40e638b1bf517a5d76b79baa80918e
a20n21 <andersonmatias188@outlook.com>
2026-07-07 21:31:18 -0300

    ajustes na imagem do frontend

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,4 +1,5 @@
-import { useState, FormEvent } from 'react';
+import { useState } from 'react';
+import type { FormEvent } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
